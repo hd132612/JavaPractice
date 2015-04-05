cb94e56ac4947eee80aae43b23cc7a0204b2ad44
중현 <@sjh-pc>
2015-04-05 20:46:00 +0900

    과제제출입니다

---
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -0,0 +1,22 @@
+package org.dimigo.basic;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<10; i++)
+		{
+			for(int j = 0; j<10; j++)
+			{
+				if(i == j) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(j+1);
+				}
+			}
+			System.out.println("");
+		}
+	}
+
+}
